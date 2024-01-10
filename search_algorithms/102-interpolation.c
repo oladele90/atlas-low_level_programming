afd6abd9e2a475826805b0e35cec075f04d32ca3
oladele90 <oladelemiro07@gmail.com>
2024-01-10 18:43:43 +0000

    interpolation search

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0, pos;
 	size_t high = size - 1;
 
-	if (!array || !size)
+	if (!array || size == 0)
 		return (-1);
 
 	while (low <= high && value >= array[low] && value <= array[high])
