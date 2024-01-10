10173ef5691ea1819d4b1597f01b0a8d40a215a8
oladele90 <oladelemiro07@gmail.com>
2024-01-10 19:04:31 +0000

    interpolation search

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -21,11 +21,6 @@ int interpolation_search(int *array, size_t size, int value)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 					* (value - array[low]));
-		if (pos > (size_t)array[high] || pos < (size_t)array[low])
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			return (-1);
-		}
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
